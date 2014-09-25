0b4577c9ee1e7f1b717416af4642702d2516a275
Vitalya Vatulya <vvatulya@brightgrove.com>
2014-09-25 16:26:04 +0000

    added simple acceptance test

--- tests/acceptance/CommonCest.php
+++ tests/acceptance/CommonCest.php
@@ -7,10 +7,20 @@ class CommonCest
     {
         $I->wantTo('check filters form on webpage');
         $I->amOnPage('/');
-//        $I->fillField('source', 'JSON');
-//        $I->fillField('source', 'PHP');
-//        $I->fillField('source', 'XML');
-        $I->fillField('filter[code]', 'XML');
+        $I->selectOption('source', 'JSON');
+        $I->fillField('filter[code]', 'USD');
+        $I->fillField('filter[group]', 'world');
+        $I->fillField('filter[name]', 'dolar');
+        $I->fillField('filter[value-range-min]', '1');
+        $I->fillField('filter[value-range-max]', '10');
+        $I->click('Submit');
+    }
+
+    public function testGetData(AcceptanceTester $I)
+    {
+        $I->wantTo('check ajax load data');
+        $I->sendAjaxGetRequest('/?action=data');
+        $I->see('id="data-table"');
     }
 
 }
